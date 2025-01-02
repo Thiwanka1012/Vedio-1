e9a0860fdc4ffd36532862629ad79cfb3f451ef0
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-01-03 00:42:45 +0530

    Usestate Hooks

--- React_Hooks/hooks/src/App.jsx
+++ React_Hooks/hooks/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 
 function App() {
   //let num1=2;
 
   const [num1, setNum1] = useState(2)
+  const[count, setcount] = useState(0)
 
   function handleClick() {
     //num1=num1+1;
@@ -12,15 +13,28 @@ function App() {
     setNum1(num1 + 1)
   }
 
+  const decrementFunction = () => {
+    setcount(count - 1)
+  }
+
+  const incrementFunction = () => {
+    setcount(count + 1)
+  }
   return (
   <>
   <div id="wrapper">
     <button onClick={handleClick}>Click</button>
     <p>{num1}</p>
 
+    <p>Counter</p>
+    <button onClick={decrementFunction}>-</button>
+    <button onClick={incrementFunction}>+</button>
+    <p>{count}</p>
+
   </div>
   </>
   )
 }
+
 
 export default App
